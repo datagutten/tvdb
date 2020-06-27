97d1881c7421e4cf8e45ccfea995e6da7988a3a1
datagutten <datagutten@datagutten.net>
2020-06-27 15:08:39 +0200

    Update example.php

--- example.php
+++ example.php
@@ -1,16 +1,21 @@
 <?Php
-require 'tvdb.php';
+
+use datagutten\tvdb\tvdb;
+use datagutten\tvdb\exceptions\api_error;
+
+require 'vendor/autoload.php';
 $tvdb=new tvdb;
-$tvdb->lang='en';
 $series='24 Hours in A&E';
 $season=3;
 $episode=15;
 // S03E15 - A Few Good Men
 //$series=$tvdb->findseries($series);
 $series=248699;
-$info = $tvdb->episode_info($series,$season,$episode);
-if($info===false)
-    echo $tvdb->error."\n";
-else
-    print_r($info);
+try {
+    $info = $tvdb->episode_info($series, $season, $episode);
+}
+catch (api_error $e) {
+    die($e->getMessage()."\n");
+}
+print_r($info);
 //print_r($tvdb->find_episode_by_name($series,'A Few Good Men'));

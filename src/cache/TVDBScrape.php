f6a0a57f5a6492ec5f43e35c9292cbdd5e71737c
datagutten <datagutten@datagutten.net>
2022-10-15 18:36:38 +0200

    Save cache files

--- src/cache/TVDBScrape.php
+++ src/cache/TVDBScrape.php
@@ -5,10 +5,12 @@ namespace datagutten\tvdb\cache;
 use datagutten\tools\files\files;
 use DOMDocument;
 use DOMXPath;
+use Symfony\Component\Filesystem\Filesystem;
 
 class TVDBScrape extends \datagutten\tvdb\TVDBScrape
 {
     public string $cache_path;
+    protected Filesystem $filesystem;
 
     public function __construct(string $cache_path)
     {
@@ -16,6 +18,7 @@ class TVDBScrape extends \datagutten\tvdb\TVDBScrape
         if (!file_exists($cache_path))
             throw new \FileNotFoundException($cache_path);
         $this->cache_path = realpath($cache_path);
+        $this->filesystem = new Filesystem();
     }
 
     public function get_xpath(string $url): DOMXPath
@@ -24,12 +27,16 @@ class TVDBScrape extends \datagutten\tvdb\TVDBScrape
         if (!empty($matches) && $matches[3] == 'episodes')
         {
             $file = $this->episode_file($matches[2], $matches[4]);
-            if (file_exists($file))
-            {
-                $dom = new DOMDocument();
+            $dom = new DOMDocument();
+            if ($this->filesystem->exists($file))
                 @$dom->loadHTMLFile($file);
-                return new DOMXPath($dom);
+            else
+            {
+                $data = $this->get($url);
+                $this->filesystem->dumpFile($file, $data);
+                @$dom->loadHTML($data);
             }
+            return new DOMXPath($dom);
         }
         return parent::get_xpath($url);
     }

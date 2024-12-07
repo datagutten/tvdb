54f12029aa85d0b34c9d3ac3413a4dda2e43a4dc
datagutten <datagutten@datagutten.net>
2024-12-07 13:29:19 +0100

    Improve URL matching for caching

--- src/cache/TVDBScrape.php
+++ src/cache/TVDBScrape.php
@@ -5,18 +5,21 @@ namespace datagutten\tvdb\cache;
 use datagutten\tools\files\files;
 use DOMDocument;
 use DOMXPath;
+use FileNotFoundException;
 use Symfony\Component\Filesystem\Filesystem;
+
 
 class TVDBScrape extends \datagutten\tvdb\TVDBScrape
 {
     public string $cache_path;
     protected Filesystem $filesystem;
+    public bool $cache_breaker = false;
 
     public function __construct(string $cache_path)
     {
         parent::__construct();
         if (!file_exists($cache_path))
-            throw new \FileNotFoundException($cache_path);
+            throw new FileNotFoundException($cache_path);
         $this->cache_path = realpath($cache_path);
         $this->filesystem = new Filesystem();
     }
@@ -24,11 +27,15 @@ class TVDBScrape extends \datagutten\tvdb\TVDBScrape
     public function get_xpath(string $url): DOMXPath
     {
         preg_match('#thetvdb\.com/(\w+)/([\w\-]+)/(\w+)/(\d+)#', $url, $matches);
+        preg_match('#/((\w+)/([\w\-]+)/(\w+)/(\w+))#', $url, $matches2);
         if (!empty($matches) && $matches[3] == 'episodes')
-        {
             $file = $this->episode_file($matches[2], $matches[4]);
+        elseif (!empty($matches2) && in_array($matches2[2], ['series']))
+            $file = $this->cache_file($matches2[1]);
+        if (!empty($file))
+        {
             $dom = new DOMDocument();
-            if ($this->filesystem->exists($file))
+            if ($this->filesystem->exists($file) && !$this->cache_breaker)
                 @$dom->loadHTMLFile($file);
             else
             {
@@ -38,7 +45,15 @@ class TVDBScrape extends \datagutten\tvdb\TVDBScrape
             }
             return new DOMXPath($dom);
         }
-        return parent::get_xpath($url);
+        else
+            return parent::get_xpath($url);
+    }
+
+    protected function cache_file($url)
+    {
+        if (DIRECTORY_SEPARATOR != '/')
+            $url = str_replace('/', DIRECTORY_SEPARATOR, $url);
+        return files::path_join($this->cache_path, $url . '.html');
     }
 
     protected function episode_file(string $series, string $id, $extension = 'html'): string
